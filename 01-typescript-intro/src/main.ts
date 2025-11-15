05aec63f3019ae2e4fd99b4ddeaede56b73fd41b
apangeles <apangeles70@gmail.com>
2025-11-15 16:57:39 -0600

    014. Basics functions

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,10 +1,10 @@
 import './style.css'
 //import './topics/01-basic-types';
-import './topics/02-object-interface';
+//import './topics/02-object-interface';
+import './topics/03-functions';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `Hola Mundo con TypeScript`;
 
-console.log("Hola Mundo!!!!!");
 
---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,22 @@
+
+
+function addNumbers(a: number, b: number): number {
+  return a + b;
+} 
+
+const addNumbersArrow =(a: number, b: number):string => { 
+    return `${a + b}` 
+};
+
+
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2) { 
+    return firstNumber *  base;
+}
+
+const result = addNumbers(1, 2); 
+const result2 = addNumbersArrow(1, 2);
+const multiplyREsult = multiply(2);
+
+console.log({ result, result2, multiplyREsult });
+
+export{};
